d726532d39c9e6f23e186706ea90b12727598f27
vaibhavjejurkar100 <vaibhavjejurkar100@gmail.com>
2025-10-19 18:09:56 +0530

    backend url

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -326,7 +326,8 @@ function Sidebar() {
 
     const getAllThreads = async () => {
         try {
-            const response = await fetch("http://localhost:8080/api/thread");
+            // const response = await fetch("http://localhost:8080/api/thread");
+            const response = await fetch("https://chatgpt-project-backend.onrender.com/api/thread");
             const res = await response.json();
             const filteredData = res.map(thread => ({threadId: thread.threadId, title: thread.title}));
             //console.log(filteredData);
@@ -352,7 +353,7 @@ function Sidebar() {
         setCurrThreadId(newThreadId);
 
         try {
-            const response = await fetch(`http://localhost:8080/api/thread/${newThreadId}`);
+            const response = await fetch(`https://chatgpt-project-backend.onrender.com/api/thread/${newThreadId}`);
             const res = await response.json();
             console.log(res);
             setPrevChats(res);
@@ -365,7 +366,7 @@ function Sidebar() {
 
     const deleteThread = async (threadId) => {
         try {
-            const response = await fetch(`http://localhost:8080/api/thread/${threadId}`, {method: "DELETE"});
+            const response = await fetch(`https://chatgpt-project-backend.onrender.com/api/thread/${threadId}`, {method: "DELETE"});
             const res = await response.json();
             console.log(res);
 

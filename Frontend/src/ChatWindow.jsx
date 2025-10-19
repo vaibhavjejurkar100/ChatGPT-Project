56c9ae0a13c043b46a4194c49caaa22e88ff60a9
vaibhavjejurkar100 <vaibhavjejurkar100@gmail.com>
2025-10-19 12:29:59 +0530

    add files

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -376,7 +376,7 @@ function ChatWindow() {
 
         try {
             // const response = await fetch("http://localhost:8080/api/chat", options);
-            const response = await fetch("https://chatgpt-project-backend.onrender.com/api/chat", options);
+            const response = await fetch("https://chatgpt-project-backend.onrender.com/", options);
             const res = await response.json();
             console.log(res);
             setReply(res.reply);

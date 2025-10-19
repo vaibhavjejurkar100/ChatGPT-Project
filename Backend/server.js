1383cac1a74b3a6733bc1046d10ce881fe9e0d5f
vaibhavjejurkar100 <vaibhavjejurkar100@gmail.com>
2025-10-19 17:59:13 +0530

    add files

--- Backend/server.js
+++ Backend/server.js
@@ -97,12 +97,10 @@ import chatRoutes from "./routes/chat.js";
 
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
-app.use(cors({
-    origin: "chatgpt-project-9lqf.onrender.com",
-}));
+app.use(cors());
 
 app.use("/api", chatRoutes);
 

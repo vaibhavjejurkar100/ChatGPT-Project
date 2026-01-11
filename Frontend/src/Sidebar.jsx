f9db44975efa0fa05f12bbd91879d49907a7e280
vaibhavjejurkar100 <vaibhavjejurkar100@gmail.com>
2026-01-11 17:21:20 +0530

    Update file

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -410,7 +410,7 @@ function Sidebar() {
             </ul>
 
             <div className="sign">
-                <p>By ApnaCollege &hearts;</p>
+                <p>By Vaibhav Jejurkar &hearts;</p>
             </div>
        </section>
     )
